921b4c989770f3514f4a52d81b1935dce27b8c3b
illshaadVinidaily <illshaad@vinidaily.com>
2023-03-13 10:40:47 +0100

    refact function

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,11 @@ const client = require("twilio")(accountSid, authToken);
 
 router.post("/sms", async (req, res) => {
   const { information, commandeForReservation, name, phone } = req.body.data;
-  const nameCommande = information.map((name) => name.products.name);
-  const typeCommande = information.map((type) => type.products.type);
-  const quantityCommande = information.map((quantity) => quantity.quantity);
-  const commandeCompleted = [];
-  for (let i = 0; i < nameCommande.length; i++) {
-    commandeCompleted.push(
-      ` ${quantityCommande[i]}  -  ${nameCommande[i]} -  ${typeCommande[i]} `
-    );
-  }
-  console.log(commandeCompleted);
+  const commandeCompleted = information.map(
+    ({ products, quantity }) =>
+      `${quantity} - ${products.name} - ${products.type}`
+  );
+
   client.messages
     .create({
       body: `Salut tu as reçu une commande de ${name} tu peux la contacter sur ${phone} - Détail de la Commande - ${commandeCompleted} -  Pour un montant ${commandeForReservation}€`,
