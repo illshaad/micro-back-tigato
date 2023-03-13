b3e16d724b03468b4341f1451b51339b8a7d8334
illshaadVinidaily <illshaad@vinidaily.com>
2023-03-13 11:17:36 +0100

    create controller

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require("twilio")(accountSid, authToken);
+const twilioCtrl = require("../controller/twilio-ctrl");
 
-router.post("/sms", async (req, res) => {
-  const { information, commandeForReservation, name, phone } = req.body.data;
-  const commandeCompleted = information.map(
-    ({ products, quantity }) =>
-      `${quantity} - ${products.name} - ${products.type}`
-  );
-
-  client.messages
-    .create({
-      body: `Salut tu as reçu une commande de ${name} tu peux la contacter sur ${phone} - Détail de la Commande - ${commandeCompleted} -  Pour un montant ${commandeForReservation}€`,
-      from: process.env.TWILIO_NUM_FROM,
-      to: process.env.TWILIO_NUM_TO,
-    })
-    .then((message) => console.log(message.sid));
-  res.status(200).send("message bien envoyé");
-});
+router.post("/sms", twilioCtrl.sendMessage);
 
 module.exports = router;
